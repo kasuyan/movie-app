12fc9440c949aaf0b8fdea33147b95c02675cbd7
kasuya_kazunori <kasuya_kazunori@cyberagent.co.jp>
2020-01-09 15:24:11 +0900

    fix:webpack.config for netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,42 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	output: {
-		filename: 'js/main.js',
-		path: path.resolve(__dirname, './dist/'),
-		publicPath: '/'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.svg$/,
-				loader: 'file-loader',
-				options: {
-					name: '[name].[ext]',
-					outputPath: './svg/',
-					publicPath: '/svg/'
+module.exports = (env, argv) => {
+	console.log('******************', env, argv);
+
+	return {
+		mode: argv.mode,
+		entry: './src/index.js',
+		output: {
+			filename: 'js/main.js',
+			path: path.resolve(__dirname, './dist/'),
+			publicPath: '/'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader'
+				},
+				{
+					test: /\.svg$/,
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: './svg/',
+						publicPath: '/svg/'
+					}
 				}
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html'
-		})
-	],
-	devServer: {
-		historyApiFallback: true
-	}
+			]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './src/index.html'
+			})
+		],
+		devServer: {
+			historyApiFallback: true
+		}
+	};
 };
